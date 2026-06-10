e56ba14bf39aa36688b72355c3a1febe445a8993
Sasipriya-03 <sasijeevitha16@gmail.com>
2026-06-10 11:06:51 +0530

    Add files via upload

---
+++ ElseIfExample.java
@@ -0,0 +1,174 @@
+package ifelse;
+
+import java.util.Scanner;
+
+public class ElseIfExample {
+
+	public static void main(String[] args) {
+		
+//	           else if anf nested if
+//			
+//			
+//		Scanner Sc=new Scanner(System.in);
+//		System.out.println("Enter a number..");
+//		int a=Sc.nextInt();
+//		int b=Sc.nextInt();
+//		System.out.println("Enter a operators");
+//		int op=Sc.next().charAt(0);
+//		if(op=='+') {
+//			System.out.println("add=" +(a+b));
+//		}
+//		else if(op=='-') {
+//			System.out.println("sub=" +(a-b));
+//				
+//		}
+//		else if(op=='*') {
+//			System.out.println("mul=" +(a*b));
+//		}
+//		else if(op=='/') {
+//			System.out.println("div=" +(a/b));
+//		}
+//		else {
+//			System.out.println("Invalid operation");
+//		}
+//			
+//			    
+//			
+//		Scanner Sc=new Scanner(System.in);
+//		System.out.println("Enter a Parcel weight...");
+//		int a=Sc.nextInt();
+//		System.out.println("Enter a Parcel type(o for ordinary, e for express)..");
+//		int b=Sc.next().charAt(0);
+//		int charge=0;
+//		if(b=='O'|| b=='o') {
+//			if(a<=100)
+//				charge=80;
+//			else if(a<=500)
+//				charge=150;
+//			else if(a<=1000)
+//				charge=210;
+//			else
+//				charge=250;
+//			
+//		}
+//		else if(b=='E'|| b=='e') {
+//			if(a<=100)
+//				charge=100;
+//			else if(a<=500)
+//				charge=200;
+//			else if(a<=1000)
+//				charge=250;
+//			else
+//				charge=300;
+//		}
+//		else {
+//			System.out.println("Invalid type..");
+//			return;
+//		}
+//		System.out.println("Parcel weight " + charge);
+		
+		
+//			
+//		Scanner Sc=new Scanner(System.in);
+//		System.out.println("Enter a number....");
+//		int a=Sc.nextInt();
+//		if(a%10==7 || a%7==0) {
+//			System.out.println("Buzz number");
+//		}
+//			
+//		else {
+//			System.out.println("Buzz number or not");
+//		}
+//			
+//			
+//			
+//		Scanner Sc=new Scanner(System.in);
+//		System.out.println("Enter a number...");
+//		int a=Sc.nextInt();
+//		int b=a*a;
+//		int c=b%10;
+//		if(a==c) {
+//			System.out.println("Automorphic number");
+//		}
+//		else {
+//			System.out.println("Automorphic number not");
+//		}
+//			
+//			
+//			
+//		Scanner Sc=new Scanner(System.in);
+//		System.out.println("Enter a letter....");
+//		char a=Sc.next().charAt(0);
+//		if(a>='A'&&a<='Z') {
+//			System.out.println("Capital letter");
+//		}
+//		else {
+//			System.out.println("Capital letter or not");
+//		}
+//			
+//			
+//			
+//		Scanner Sc=new Scanner(System.in);
+//		System.out.println("Enter a Price of laptop...");
+//		double price,discount,totalprice;
+//	    price =Sc.nextDouble();
+//		if(price<=50000) {
+//		    discount=0;
+//		}
+//		else if(price<=100000) {
+//			discount=price*0.10;
+//				
+//		}
+//		else if(price<=150000) {
+//			discount=price*0.15;
+//		}
+//		else {
+//			discount=price*0.20;
+//		}
+//		totalprice=price-discount;
+//		System.out.println("Price of laptop:" +price);
+//		System.out.println("Discount:" +discount);
+//		System.out.println("Total price:" +totalprice);
+//			
+//			
+//			
+//		Scanner Sc=new Scanner(System.in);
+//		System.out.println("Enter number of late days...");
+//		int days=Sc.nextInt();
+//		double fine;
+//		if(days<=5) {
+//			fine=days*0.40;
+//		}
+//		else if(days<=10) {
+//			fine=days*0.65;
+//				
+//		}
+//		else {
+//			fine=days*0.80;
+//		}
+//		System.out.println("Fine=Rs." +fine);
+		
+		
+//			
+		
+//		Scanner Sc=new Scanner(System.in);
+//		System.out.println("Enter a user name...");
+//		String name=Sc.nextLine();
+//		if(name.equals("Java")) {
+//			System.out.println("Enter a password..");
+//			String pass=Sc.nextLine();
+//			if(pass.equals("12345")) {
+//				System.out.println("Login Success");
+//			}
+//			else {
+//				System.out.println("Incorrect Password");
+//			}
+//		}
+//		else {
+//			System.out.println("Invalid user name");
+//		}
+		
+
+	}
+
+}
