791bc27d577a7ba3659dacc86937a8ec5b8a2e5d
Sasipriya-03 <sasijeevitha16@gmail.com>
2026-06-10 12:29:04 +0530

    Add files via upload

---
+++ ForLoppExample.java
@@ -0,0 +1,159 @@
+package forloop;
+import java.util.Scanner;
+
+public class ForLoppExample {
+
+	public static void main(String[] args) {
+		
+
+		
+//		int sum=0;
+//		System.out.println("First 10 natural number..");
+//		for(int i=0;i<=10;i++) {
+//			sum=sum+i;
+//			System.out.println(sum);
+//		}
+//		System.out.println("First 10 natural number..");
+//		System.out.println(sum);
+		
+		
+//		
+//		Scanner Sc=new Scanner(System.in);
+//		System.out.println("Enter a number...");
+//		int a=Sc.nextInt();
+//		int n=0,n1=1;
+//		System.out.println(n+"\n"+n1);
+//		for(int i=0;i<=a;i++) {
+//			int n2=n+n1;
+//		    n=n1;
+//			n1=n2;
+//			System.out.println(n2);
+//		}
+		
+		
+//		          pattern
+//		
+//		for(int i=1;i<=5;i++) {
+//			for(int j=1;j<=i;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+//		for(int i=1;i<=5;i++) {
+//			for(int j=1;j<=i;j++) {
+//				System.out.print(i+" ");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+//		for(int i=1;i<=5;i++) {
+//			for(int j=1;j<=i;j++) {
+//				System.out.print(j+" ");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+//		int a=65;
+//		for(int i=1;i<=7;i++){
+//			for(int j=1;j<=i;j++) {
+//				System.out.print((char)a+" ");
+//				a++;
+//				if(a>90) {
+//					a=65;
+//				}
+//			}
+//			System.out.println();
+//		}
+		
+		
+//		Scanner Sc=new Scanner(System.in);
+//		int evensum=0;
+//		int oddsum=0;
+//		System.out.println("How many number...");
+//		int n=Sc.nextInt();
+//		for(int i=1;i<=n;i++) {
+//			System.out.println("Enter a number...");
+//			int num=Sc.nextInt();
+//			if(num%2==0)
+//				evensum+=num;
+//			else
+//				oddsum+=num;
+//			
+//		}
+//		System.out.println("Evensum="+evensum);
+//		System.out.println("Oddsum="+oddsum);
+		
+//		
+//		
+//		Scanner Sc=new Scanner(System.in);
+//		System.out.println("Enter a number...");
+//		int a=Sc.nextInt();
+//		if(a>0) {
+//			for(int i=1;i<=10;i++) {
+//				System.out.println(i+"x"+a+"="+(a*i));
+//			}
+//			
+//		}
+//		else {
+//			System.out.println("Enter a positive number");
+//		}
+		
+//		
+//		Scanner Sc=new Scanner(System.in);
+//		System.out.println("Enter a number...");
+//		int n=Sc.nextInt();
+//		int fact=1;
+//		for(int i=1;i<=n;i++) {
+//			fact*=i;
+//			
+//		}
+//		System.out.println("Factorial="+fact);
+		
+		
+//		
+//		for(char ch='A'; ch<='Z';ch++) {
+//			System.out.println(ch +" ");
+//		}
+//		
+		
+//		for(char ch='z';ch>='a';ch--) {
+//			System.out.println(ch +" ");
+//		}
+		
+//		
+//		for(int i=0;i<=127;i++) {
+//			System.out.println(i+"="+(char)i);
+//		}
+		
+//		
+//		int m=5;
+//		for(int n=10;n>=1;n--) {
+//			System.out.println(m*n);
+//		}
+		
+//		
+//		for(int i=1;i<=4;i++) {
+//			for(int j=1;j<=i;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		
+//		
+//		for(int i=1;i<=5;i++) {
+//			for(int j=1;j<=i;j++) {
+//				System.out.print(i+" ");
+//			}
+//			System.out.println();
+//		}
+//		
+	}
+
+}
