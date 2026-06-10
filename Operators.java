9923b0e0979280c13cefa0621ccd85ff1a5d3fdd
Sasipriya-03 <sasijeevitha16@gmail.com>
2026-06-10 10:32:50 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,81 @@
+package operators;
+
+import java.util.Scanner;
+
+
+public class Operators {
+
+	public static void main(String[] args) {
+//		System.out.println("Hello java");
+//		
+//        Operators
+//	
+//	Scanner Sc=new Scanner(System.in);
+//	System.out.println("Enter a value...");
+//	int a=Sc.nextInt();
+//	int b=Sc.nextInt();
+//	a=a^b;
+//	b=a^b;
+//	a=a^b;
+//	System.out.println("a=" +a);
+//	System.out.println("b=" +b);
+//	
+//	
+//	Scanner Sc=new Scanner(System.in);
+//	System.out.println("Enter a number...");
+//	int a=Sc.nextInt();
+//	int b=a%100;
+//	System.out.println(b);
+//	
+//	
+//	Scanner Sc=new Scanner(System.in);
+//	System.out.println("Enter a number...");
+//	int a=Sc.nextInt();
+//	System.out.println((a/100)%10);
+//	
+//	
+//	Scanner Sc=new Scanner(System.in);
+//	System.out.println("Enter a number....");
+//	int a=Sc.nextInt();
+//	int b=(a/100)%10;
+//	int c=b*b;
+//	System.out.println(c);
+//	
+//	
+//	Scanner Sc=new Scanner(System.in);
+//	System.out.println("Enter a number....");
+//	int a=Sc.nextInt();
+//	System.out.println(a%10);
+//	
+//	
+//	int a=45;
+//	int b=34;
+//	int c=a+b;
+//	System.out.println("a+b=" +c);
+//	
+//	
+//	int a=45;
+//	int b=67;
+//	System.out.println("Sum=" +(a+b));
+//	System.out.println("difference=" +(a-b));
+//	System.out.println("Product=" +(a*b));
+//	System.out.println("Divide=" +(a/b));
+//	System.out.println("Mod=" +(a%b));
+//	
+//	
+//	int h=170;
+//	int w=60;
+//	int bmi=(w/(h/100)^2);
+//	System.out.println(bmi);
+//	
+//	
+//	int a=1234;
+//	int b=a/1000;
+//	int c=(a/100)%10;
+//	int d=(a/10)%10;
+//	int e=a%10;
+//	System.out.println("Expand form=" +(b*1000) + "+" +(c*100) + "+" +(d*10) + "+" +(e));
+
+	}
+
+}
