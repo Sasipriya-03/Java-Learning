d8a1e3b92c30f60125d37bc7f776a60cc37c1144
Sasipriya-03 <sasijeevitha16@gmail.com>
2026-06-10 12:38:32 +0530

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,90 @@
+package array;
+
+import java.util.Scanner;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		
+		
+//		Scanner Sc=new Scanner(System.in);
+//	    int arr[]=new int[5];
+//	    int sum=0;
+//	    System.out.println("Enetr a 5 Elements");
+//	    for(int i=0;i<5;i++) {
+//	    	arr[i]=Sc.nextInt();
+//	    }
+//	    int largest=0;
+//	    int smallest=0;
+//	    for(int i=0;i<5;i++) {
+//	    	sum=sum+arr[i];
+//	    	if(arr[i]>largest) {
+//	    		largest=arr[i];
+//	    	}
+//	    	if(arr[i]<smallest) {
+//	    		smallest=arr[i];
+//	    	}
+//	    }
+//	    System.out.println("Largest=" +largest);
+//	    System.out.println("Smallest=" +smallest);
+//	    System.out.println("Sum of Elements=" +sum);
+//	    
+//		
+//		
+//		Scanner Sc=new Scanner(System.in);
+//		System.out.println("Enter a number of Students...");
+//		int n=Sc.nextInt();
+//		int roll[]=new int[n];
+//		String name[]=new String[n];
+//		int mar1[]=new int[n];
+//		int mar2[]=new int[n];
+//		int mar3[]=new int[n];
+//		for(int i=0;i<n;i++) {
+//			System.out.println("Enter  a Roll no");
+//			roll[i]=Sc.nextInt();
+//			System.out.println("Enter a name");
+//			name[i]=Sc.next();
+//			System.out.println("Ente a mark1");
+//			mar1[i]=Sc.nextInt();
+//			System.out.println("Ente a mark2");
+//			mar2[i]=Sc.nextInt();
+//			System.out.println("Ente a mark3");
+//			mar3[i]=Sc.nextInt();
+//		}
+//		System.out.println("Enter a Students details");
+//		for(int i=0;i<n;i++) {
+//			double avg=(mar1[i]+mar2[i]+mar3[i]);
+//			String remark;
+//			if(avg>=85)
+//				remark="EXCELLENT";
+//			else if(avg>=75)
+//				remark="DISTINCTION";
+//			else if(avg>=60)
+//				remark="FIRST CLASS";
+//			else if(avg>=40)
+//				remark="PASS";
+//			else
+//				remark="POOR";
+//			System.out.println(roll[i]+" "+name[i]+"Avg="+avg+" "+remark);
+//				
+//		}
+//		
+//		
+//		
+//		int arr[]= {10,20,30,40,50};
+//		int max=arr[0];
+//		int min=arr[0];
+//		for(int i=0;i<arr.length;i++) {
+//			if(arr[i]>max)
+//				max=arr[i];
+//			if(arr[i]<min)
+//				min=arr[i];		
+//		}
+//      System.out.println("Maximum=" +max);
+//		System.out.println("Minimun=" +min);
+//		
+//		
+
+	}
+
+}
