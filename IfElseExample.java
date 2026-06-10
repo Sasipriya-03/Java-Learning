b718167f02ced4584df626d79647526e1ad42dd6
Sasipriya-03 <sasijeevitha16@gmail.com>
2026-06-10 10:41:26 +0530

    Add files via upload

---
+++ IfElseExample.java
@@ -0,0 +1,114 @@
+package ifelse;
+
+import java.util.Scanner;
+
+public class IfElseExample {
+
+	public static void main(String[] args) {
+		
+		
+//            if else
+//    		
+//	Scanner Sc=new Scanner(System.in);
+//	int l=Sc.nextInt();
+//	int b=Sc.nextInt();
+//	if(l==b) {
+//		System.out.println(" it’s a square ");
+//	}
+//	else {
+//		System.out.println(" it’s a rectangular");
+//	}
+//	
+//	
+//	
+//	Scanner Sc=new Scanner(System.in);
+//	System.out.println("Enter a number...");
+//	int a=Sc.nextInt();
+//	int b=a*a;
+//	if(b>=50) {
+//		System.out.println("Above 50");
+//	}
+//	else {
+//		System.out.println("Below 50");
+//	}
+//	
+//	
+//	
+//	Scanner Sc=new Scanner(System.in);
+//	System.out.println("Enter a number....");
+//	int a=Sc.nextInt();
+//	int b=Sc.nextInt();
+//	int c=a-b;
+//	if(c==0) {
+//		System.out.println("It's Zero");
+//	}
+//	else {
+//		System.out.println("Not Zero");
+//	}
+//	
+//	
+//	
+//	Scanner Sc=new Scanner(System.in);
+//	System.out.println("Enter a CS mark....");
+//	int mark=Sc.nextInt();
+//	if(mark>=50) {
+//		System.out.println("Pass");
+//	}
+//	else {
+//		System.out.println("Fail");
+//	}
+//	
+//	
+//	
+//	Scanner Sc=new Scanner(System.in);
+//	System.out.println("Enter a number...");
+//	int a=Sc.nextInt();
+//	if(a%10==0) {
+//		System.out.println("Divisible by 10");
+//	}
+//	else {
+//		System.out.println("Not Divisible by 10");
+//	}
+//	
+//	
+//	
+//	Scanner Sc=new Scanner(System.in);
+//	System.out.println("Enter a two digit number...");
+//	int a=Sc.nextInt();
+//	int b=a%10;
+//	int c=a/10;
+//	if(b>c) {
+//		System.out.println("biggest digit");
+//	}
+//	else {
+//		System.out.println("not biggest digit");
+//	}
+//	
+//	
+//	
+//	Scanner Sc=new Scanner(System.in);
+//	System.out.println("Enter a number...");
+//	int a=Sc.nextInt();
+//	if(a==1) {
+//		System.out.println("You can go out and play");
+//	}
+//	else {
+//		System.out.println("You cannot go out and play");
+//	}
+//	
+//	
+//	
+//	Scanner Sc=new Scanner(System.in);
+//	System.out.println("Enter a number...");
+//	int a=Sc.nextInt();
+//	if(a==1) {
+//		System.out.println("The exam will be easy");
+//	}
+//	else {
+//		System.out.println("The exam will be diffcult");
+//	}
+//	
+
+	}
+
+}
