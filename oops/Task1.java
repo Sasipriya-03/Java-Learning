247a66572f3a4dd097c2886c8b24c1550ca8ac27
Sasipriya D <sasijeevitha16@gmail.com>
2026-06-16 14:54:49 +0530

    Added OOPs Program

---
+++ oops/Task1.java
@@ -0,0 +1,20 @@
+package oops;
+class car{
+	String color="blue";
+	void drive() {
+		System.out.println("We have long drive with my " + color + " color car");
+	}
+	
+}
+
+public class Task1 {
+	
+
+	public static void main(String[] args) {
+		car c=new car();
+		c.drive();
+		
+
+	}
+
+}
